808b8fe3fa871744d82b157393b5390c20361f46
PandaBanda121 <panda121banda121@gmail.com>
2024-07-20 22:45:50 -0500

    using getch and not using any GUI (idk how to T_T), and finding good characters to make boxes in terminal text based

--- game.cpp
+++ game.cpp
@@ -5,43 +5,54 @@
 #include <algorithm>
 #include <math.h>
 #include <cstdlib>
+#include <conio.h>
 
 //testing
 #include <Windows.h>
-
-
 
 #include "round.h"
 
 using namespace std;
 
-string choice;
+int input;
 int N = 20;
 double current=10, mult=1;
 int num1, num2;
 double deposit = 0;
-bool gameFinish = false;
 bool winRound = false;
-bool lost = false;
+bool winGame = false;
+bool lostGame = false;
+bool quitGame = false;
+
+void checkQuit() {
+    if(input == 13) {
+        cout << "Are you sure you want to quit?" << endl;
+        input = getch();
+        if(input == 13) quitGame = true;
+    }
+}
+
+
 void intro() {
     cout << "Welcome! This game is a game of risk taking, both high and low." << endl;
     cout << "[1] Game Background" << endl;
     cout << "[2] How to Play" << endl;
-    cout << "[3] Start" << endl;
-    string s;
-    cin >> s;
-    while(s != "3") {
-        if(s=="1") {
+    cout << "[3] Start" << endl << endl;
+    input = getch();
+    checkQuit();
+    if(quitGame) return;
+    while(input != 51) {
+        if(input == 49) {
             cout << "~~==Game Background==~~" << endl;
             cout << "Wow, you're actually reading this." << endl;
-            cout << "Well, Panda Banda (developer) created this game in the summer of 2024, because he was bored." << endl;
-            cout << "Anyways, enjoy the game." << endl << endl;
+            cout << "Well, Panda (developer) created this game in the summer of 2024, because he was bored." << endl;
+            cout << "Anyways, enjoy the game. (And there's a secret)" << endl << endl;
             cout << "~~==MENU==~~" << endl;
             cout << "[1] Game Background" << endl;
             cout << "[2] How to Play" << endl;
-            cout << "[3] Start" << endl;
-            cin >> s;
-        } else if(s=="2") {
+            cout << "[3] Start" << endl << endl;
+            input = getch();
+        } else if(input == 50) {
             cout << "~~==How to Play==~~" << endl;
             cout << "Initial info: You start with 10 coins and you need to reach a quota before you finish the round" << endl;
             cout << "Initial info: In each round, you start off with how many coins you had last round" << endl;
@@ -53,11 +64,8 @@ void intro() {
             cout << "~~==MENU==~~" << endl;
             cout << "[1] Game Background" << endl;
             cout << "[2] How to Play" << endl;
-            cout << "[3] Start" << endl;
-            cin >> s;
-        } else {
-            cout << "Invalid input, try again." << endl;
-            cin >> s;
+            cout << "[3] Start" << endl << endl;
+            input = getch();
         }
     }
     cout << "Alright, let the risks begin!" << endl << endl;
@@ -65,32 +73,32 @@ void intro() {
 
 void genTurn(Round *rou) {
     num1 = rand()%N+1;
+    while(num1 == 1 || num1 == N) num1 = rand()%N+1;
     num2 = rand()%N+1;
-    while(num1 == num2) num2 = rand()%N+1;
+    while(num1 == num2 || num2 == 1 || num2 == N) num2 = rand()%N+1;
     cout << "The first number is " << num1 << ". " << num2 << endl;
 }
 
 void makeMove(Round *rou) {
-    cout << "Enter h/H or l/L for higher or lower." << endl;
-    cin >> choice;
-    bool win = ( (choice=="H"||choice=="h") && num2>num1 ) || ( (choice=="L"||choice=="l") && num2<num1 );
-    if(choice == "H" || choice == "h") {
-        if(num2>num1) cout << "Congrats, it was higher (" << num2 << ")" << endl;
-        else cout << "Womp womp it was lower (" << num2 << ")" << endl;
-    } else if(choice == "L" || choice == "l") {
-        if(num2<num1) cout << "Congrats, it was lower (" << num2 << ")" << endl;
-        else cout << "Womp womp it was higher (" << num2 << ")" << endl;
-    } else {
-        cout << "Bro invalid input. ";
-        makeMove(rou);
+    if(!quitGame) {
+        cout << "Enter h/H or l/L for higher or lower." << endl;
+        input = getch();
+        checkQuit();
+
+        bool winTurn = ( (input == 104 || input == 72) && num2>num1 ) || ( (input == 108 || input == 76) && num2<num1 );
+        if(input == 104 || input == 72) {
+            if(num2>num1) cout << "Congrats, it was higher (" << num2 << ")" << endl;
+            else cout << "Womp womp it was lower (" << num2 << ")" << endl;
+        } else if(input == 108 || input == 76) {
+            if(num2<num1) cout << "Congrats, it was lower (" << num2 << ")" << endl;
+            else cout << "Womp womp it was higher (" << num2 << ")" << endl;
+        }
+        double multAdd = (double) num1/N;
+        if(input == 108 || input == 76) multAdd = 1.00-multAdd;
+        if(!winTurn) multAdd = -multAdd;
+        mult = mult+multAdd;
+        cout << "You now have " << (current*mult) << endl << endl;
     }
-    double multAdd = (double) num1/N;
-    if(choice=="l" || choice == "L") multAdd = 1.00-multAdd;
-    if(!win) multAdd = -multAdd;
-    mult = mult+multAdd;
-    cout << "You now have " << (current*mult) << endl << endl;
-
-
 }
 
 void checkDeposit(Round *rou) {
@@ -98,12 +106,12 @@ void checkDeposit(Round *rou) {
         cout << "You reached the quota for this round, play it safe and deposit all or keep the risks going?" << endl;
         cout << "If you keep going, you can earn more before next round, or win this round first." << endl;
         cout << "Enter y/Y to deposit, or n/N to keep going" << endl;
-        cin >> choice;
-        while(choice != "y" && choice != "Y" && choice != "n" && choice != "N") {
+        input = getch();
+        while(input != 121 && input != 89 && input != 110 && input != 78) {
             cout << "Bro invalid input." << endl;
-            cin >> choice;
+            input = getch();
         }
-        if(choice == "y" || choice == "Y") {
+        if(input == 121 || input == 89) {
             cout << "Congratulations! You finished round #" << rou->roundNum << ". Entering next round." << endl;
             deposit = deposit + rou->quota;
             current = mult*current;
@@ -112,8 +120,8 @@ void checkDeposit(Round *rou) {
             cout << "Deposit: " << deposit << endl;
             cout << "Current: " << current << endl;
             winRound = true;
-        } else if (choice == "n" || choice == "N") {
-            cout << "Alrighty, keep the risks rolling!" << endl;
+        } else if (input == 110 || input == 78) {
+            cout << "Alrighty, keep the risks rolling." << endl;
             winRound = false;
         }
     }
@@ -128,6 +136,7 @@ void genRound(Round *rou) {
     for(int turnNum = 1; turnNum <= rou->turns; turnNum++) {
         cout << "Turn #" << turnNum << endl;
         genTurn(rou);
+        if(quitGame) break;
         makeMove(rou);
         if(turnNum == rou->turns) {
             cout << "Aight, you just finished your last move for this turn, time to end it" << endl;
@@ -137,37 +146,60 @@ void genRound(Round *rou) {
     if(!winRound) {
         if(current*mult < rou->quota) {
             cout << "Womp womp, you lost. Try harder next time :(" << endl << endl;
-            lost = true;
+            lostGame = true;
         } else {
             cout << "Wow, played until the last round to win huh, time to go to the next round" << endl << endl;
         }
     }
 }
 
+
 int main() {
     srand(time(0));
     intro();
 
+    int rListSize = 7;
+    Round *rList[rListSize];
+    Round *r1 = new Round(), *r2 = new Round(), *r3 = new Round(), *r4 = new Round(), *r5 = new Round(), *r6 = new Round(), *r7 = new Round();
+    r1->roundNum = 1, r1->quota = 13, r1->turns = 10;
+    r2->roundNum = 2, r2->quota = 5, r2->turns = 10;
+    r3->roundNum = 3, r3->quota = 8, r3->turns = 7;
+    r4->roundNum = 4, r4->quota = 15, r4->turns = 6;
+    r5->roundNum = 5, r5->quota = 20, r5->turns = 5;
+    r6->roundNum = 6, r6->quota = 24, r6->turns = 5;
+    r7->roundNum = 6, r7->quota = 25, r7->turns = 4;
+    rList[0] = r1;
+    rList[1] = r2;
+    rList[2] = r3;
+    rList[3] = r4;
+    rList[4] = r5;
+    rList[5] = r6;
+    rList[6] = r7;
 
 
-
-    Round *r1 = new Round(), *r2 = new Round(), *r3 = new Round(), *r4 = new Round(), *r5 = new Round();
-    r1->roundNum = 1, r1->quota = 12, r1->turns = 10;
-    r2->roundNum = 2, r2->quota = 15, r2->turns = 10;
-    r3->roundNum = 3, r3->quota = 18, r3->turns = 7;
-    r4->roundNum = 4, r4->quota = 25, r4->turns = 6;
-    r5->roundNum = 5, r5->quota = 43, r5->turns = 5;
-    // cout << r1->roundNum << " " << r1->quota << " " << r1->turns << endl;
     genRound(r1);
-    if(lost) return 0;
+    if(lostGame) return 0;
     genRound(r2);
-    if(lost) return 0;
+    if(lostGame) return 0;
     genRound(r3);
-    if(lost) return 0;
+    if(lostGame) return 0;
     genRound(r4);
-    if(lost) return 0;
-    cout << "Game finished, type a string and hit enter to quit";
-    string s;
-    cin >> s;
+    if(lostGame) return 0;
+    genRound(r5);
+    if(lostGame) return 0;
+    cout << "Wow... you won? Congratulations huh... You did it :)" << endl;
+    cout << "Game finished, hit enter to quit";
+    while(input != 13) input = getch();
     return 0;
 }
+
+/*
+
+130
+202
+413
+630
+830
+1026
+
+*/
